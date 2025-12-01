4ed61d1dab01843b423ab9a44f404657f5105b5a
Sindre Setran <sindre.setran@gmail.com>
2025-12-01 13:15:28 +0100

    Homepage hero updates

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -24,6 +24,7 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-4.5 has-[>svg]:px-4 rounded-full",
+        xl: "h-12 rounded-md px-6 has-[>svg]:px-5 rounded-full text-base",
         icon: "size-9",
         "icon-sm": "size-8",
         "icon-lg": "size-10",
